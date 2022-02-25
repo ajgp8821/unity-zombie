962b4fae5273ef27ec5fceeb0010442929d2aca5
ajgp8821 <ajgp8821@gmail.com>
2022-02-25 18:31:26 -0300

    Zombie attack animation

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -16,20 +16,34 @@ public class ZombieController : MonoBehaviour
 
     Vector3 localScale;
 
+    public static bool isAttacking = false;
+
+    Animator anim;
+
     private void Start() {
         localScale = transform.localScale;
         rb = GetComponent<Rigidbody2D>();
         dirX = -1f;
+
+        anim = GetComponent<Animator>();
     }
     private void Update() {
         if (transform.position.x < -9f)
             dirX = 1f;
         else if (transform.position.x > 9f)
             dirX = -1f;
+
+        if (isAttacking)
+            anim.SetBool("isAttacking", true);
+        else
+            anim.SetBool("isAttacking", false);
     }
 
     private void FixedUpdate() {
-        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        if (!isAttacking)
+            rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        else
+            rb.velocity = Vector2.zero;
     }
 
     private void LateUpdate() {
